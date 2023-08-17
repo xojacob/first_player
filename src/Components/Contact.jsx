c9867d8f44240d1c35188e65a8d576c7ac58d971
bigchungus6 <92024032+bigchungus6@users.noreply.github.com>
2023-08-17 11:25:49 +1000

    Contact finish
    Finished contact section by adding links to external pages: Artstation, LinkedIn.

--- src/Components/Contact.jsx
+++ src/Components/Contact.jsx
@@ -1,11 +1,12 @@
 import React from "react";
 import styles from "../style";
+import { linkedin, artstation } from "../assets";
 const Contact = () => {
   return (
     <section id="contact" className=" w-full h-full bg-primary pb-10">
       <div className="max-w-[1400px] w-full pt-40 mx-auto">
         <div className="contact-card sm:w-1/2 rounded-3xl sm:min-w-[600px] min-w-[300px]">
-          <div className="content-above-gradient bg-[#43397639] backdrop-blur-lg rounded-3xl p-5">
+          <div className="content-above-gradient bg-[#43397639] backdrop-blur-lg rounded-3xl p-5 border-2 border-white border-opacity-5">
             <div>
               <h1
                 className={`${styles.contactGradient} font-martian sm:text-5xl text-4xl font-bold pb-10 text-transparent bg-clip-text z-[1] drop-shadow-md`}
@@ -13,15 +14,38 @@ const Contact = () => {
                 Contact.
               </h1>
             </div>
-            <div className="pt-4 flex">
+            <div className=" flex">
               <h1 className="font-roboto text-2xl text-[#7081bb] sm:w-full transition-all text-center">
                 Let's get in touch...
               </h1>
             </div>
-            <div className="font-martian sm:text-2xl text-lg py-20 text-center drop-shadow-lg">
+            <div className="font-martian sm:text-2xl text-lg pt-10 text-center drop-shadow-lg">
               <a href="mailto:jwj.smith98@gmail.com" className="contact-email">
                 jwj.smith98@gmail.com
               </a>
+            </div>
+            <div className="flex justify-center pb-5 pt-14 bg-clip-content">
+              <div className="w-[50px] mx-5 linkedin-mask" href="">
+                <a
+                  href="https://www.linkedin.com/in/jacob-smith-20b559202/"
+                  target="_blank"
+                  rel="noopener noreferrer"
+                >
+                  <div className="linkedin-gradient-mask" />
+                  <div className="linkedin-logo-image" />
+                </a>
+              </div>
+
+              <div className="w-[50px] h-[50px] mx-5 artstation-mask">
+                <a
+                  href="https://www.artstation.com/jacobwjsmith"
+                  target="_blank"
+                  rel="noopener noreferrer"
+                >
+                  <div className="artstation-gradient-mask" />
+                  <div className="artstation-logo-image" />
+                </a>
+              </div>
             </div>
           </div>
         </div>
--- src/assets/index.js
+++ src/assets/index.js
@@ -4,4 +4,15 @@ import menu from "./menu.svg";
 import XOSClogo from "./XOSC-Logo.png";
 import analogascension from "./analogascension.png";
 import starthere from "./starthere.jpg";
-export { logo, close, menu, XOSClogo, analogascension, starthere };
+import artstation from "./artstationstroke.png";
+import linkedin from "./linkedinstroke.png";
+export {
+  logo,
+  close,
+  menu,
+  XOSClogo,
+  analogascension,
+  starthere,
+  linkedin,
+  artstation,
+};
