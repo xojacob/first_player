a8dac23c066e7f17e725378e2da341c5354a2d03
bigchungus6 <jwj.smith98@gmail.com>
2023-08-20 15:39:25 +1000

    Update Hero.jsx

--- src/Components/Hero.jsx
+++ src/Components/Hero.jsx
@@ -9,7 +9,7 @@ const Hero = () => {
           <div className={`hero z-0 overflow-hidden w-full h-full flex`}>
             <div className="w-full h-full font-martian font-bold absolute flex-row flex">
               <div className="sm:w-1/6 w-0" />
-              <div className="z-20 flex-col w-full my-auto mx-auto text-left items-center justify-center lg:text-xl md:text-lg text-md xs:text-xs">
+              <div className="z-20 flex-col w-full my-auto mx-auto text-left items-center justify-center lg:text-xl md:text-lg text-md text-xs">
                 <h1 className="bg-clip-text text-transparent hover:text-[#ddddddd8] hover:bg-clip-border  bg-gradient-to-l from-[#d55b5b3e] to-[#decc8ede] border-t-2 border-b-2 border-[#bba4a132] relative overflow-hidden ">
                   Web Dev
                 </h1>
