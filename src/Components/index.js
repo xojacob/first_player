3804201a7cf911467c9526ad8273b7638752373f
bigchungus6 <jwj.smith98@gmail.com>
2023-08-30 00:06:13 +1000

    Create index.js

---
+++ src/Components/index.js
@@ -0,0 +1,20 @@
+import Navbar from "./Navbar";
+import Artwork from "./Artwork";
+import Blog from "./Blog";
+import ComputerScience from "./ComputerScience";
+import Music from "./Music";
+import MyProjects from "./MyProjects";
+import ProjectDetails from "./ProjectDetails";
+import Contact from "./Contact";
+import Hero from "./Hero";
+export {
+  Contact,
+  Navbar,
+  Artwork,
+  Blog,
+  ComputerScience,
+  Music,
+  MyProjects,
+  ProjectDetails,
+  Hero,
+};
