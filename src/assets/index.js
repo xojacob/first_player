12f4cff964d4567be34301af51d976ca7ec612df
bigchungus6 <jwj.smith98@gmail.com>
2023-08-31 16:30:12 +1000

    Image resize and 404 fix

--- src/assets/index.js
+++ src/assets/index.js
@@ -2,7 +2,7 @@ import logo from "./logo.svg";
 import close from "./close.svg";
 import menu from "./menu.svg";
 import XOSClogo from "./XOSC-Logo.png";
-import analogascension from "./analogascension.png";
+import analogascension from "./analogascension.jpg";
 import starthere from "./starthere.jpg";
 import artstation from "./artstationstroke.png";
 import linkedin from "./linkedinstroke.png";
